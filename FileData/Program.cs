d3f1c4148f3365ac5e0fd2b8a333c9ffee6a3d36
sradnus <48505312+sradnus@users.noreply.github.com>
2019-03-13 12:05:24 +0530

    Add files via upload

---
+++ FileData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+using System.Text;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+			
+        }
+		
+		public static string GetFileDetails(string arg1,string arg2)
+		{
+		    FileDetails fs = new FileDetails();
+			if(System.Text.RegularExpressions.Regex.IsMatch(arg1,"^[-v/v-V/V]"))
+			{
+			    return fs.Version(arg2);	
+			}
+			else if(System.Text.RegularExpressions.Regex.IsMatch(arg1,"^[-s/s-S/S]"))
+			{
+			    return fs.Size(arg2);
+			}
+			else
+				return "";
+		}
+		
+    }
+}
